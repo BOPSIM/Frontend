38d0b042d9c2aef086a6c2182919d16bf2211dd2
jhlee-danbi <jh.lee@danbicorp.com>
2022-05-23 20:16:16 +0900

    Add: styled-component, jsconfig.json

--- src/Views/Login/index.jsx
+++ src/Views/Login/index.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function Login() {
-  return <>로그인</>;
+  return (
+    <>
+      <Title>스타일 컴포넌트 예시</Title>
+    </>
+  );
 }
+
+const Title = styled.h1`
+  font-size: 1.5em;
+`;
