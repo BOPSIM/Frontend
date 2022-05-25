b0d5b5904c107b7943f3bef6f67ea6fd077f0850
LJH <049494@ewhain.net>
2022-05-26 00:43:13 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "Views/Login";
+import EmailLogin from "Views/Login/email_login";
+import KakaoLogin from "Views/Login/kakao_login";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/login" element={<KakaoLogin />} />
+          <Route path="/login" element={<EmailLogin />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Views/Login/email_login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function EmailLogin() {
+  return (
+    <>
+      <input type="email"></input>
+      <input type="password"></input>
+    </>
+  );
+}
